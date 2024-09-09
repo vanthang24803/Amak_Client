adf24b9e386ea33b189fcada6e28cec15dab9b1d
vanthang240803 <nguyenthang240803@gmail.com>
2024-09-09 16:03:53 +0700

    Refactor fetch api analytic

--- app/(root)/(dashboard)/dashboard/accounts/components/table.tsx
+++ app/(root)/(dashboard)/dashboard/accounts/components/table.tsx
@@ -1,38 +1,20 @@
 "use client";
 
-import { UserAnalytic, Response } from "@/types";
 import _http from "@/utils/http";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Loading } from "../../_components/loading";
 import { DataTable } from "./data-table";
 import { columns } from "./columns";
+import { useAccountAnalytic } from "@/hooks/analytic/use-account";
 
 export const AccountTable = () => {
-  const [data, setData] = useState<Response<UserAnalytic[]>>();
-  const [loading, setLoading] = useState<boolean>(false);
-
-  const fetchData = async () => {
-    try {
-      setLoading(true);
-      const response = await _http.get(`/Analytic/Accounts`);
-
-      if (response.status === 200) {
-        setData(response.data);
-      }
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
-  };
+  const { data, fetchAccounts, loading } = useAccountAnalytic();
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    fetchAccounts();
+  }, [fetchAccounts]);
 
   if (loading) return <Loading />;
 
-  return (
-    <DataTable searchKey="email" columns={columns} data={data?.result ?? []} />
-  );
+  return <DataTable searchKey="email" columns={columns} data={data ?? []} />;
 };
